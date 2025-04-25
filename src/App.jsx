0d28fcdf54691a4036fe0fc17145f51d0e5c8f6d
Cselfy <cselfymain@gmail.com>
2025-04-25 20:32:40 +0300

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,3 @@ function App() {
 }
 
 export default App;
-
-// npm install react-typed --save
-// npm install react-router-dom bootstrap-icons
-// npm install i18next react-i18next i18next-browser-languagedetector
